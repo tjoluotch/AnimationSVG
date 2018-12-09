23456beb72ebfca121fde2ffabcdd89160426f6e
TJ Oluotch <trevorjoluotch@gmail.com>
2018-12-09 13:06:49 +0000

    Triangles on multiple levels moving on repeat

--- MyAnimation.hs
+++ MyAnimation.hs
@@ -5,27 +5,81 @@ import Animation
 -- remeber (0,0) otherwise known as the origin is always at the top left of the picture
 picture :: Animation
 
--- picture = withPaint (always blue) (combine [translate (always (50, 50*i)) (rect (always (50*i)) (always 40)) | i <- [1..10]])
+picture =
+    triLimeLTR
+    `plus`
+    triLimeRTL
+    `plus`
+    triTealLTR
+    `plus`
+    triTealRTL
 
-
-picture = 
-    (translate
-        (cycleSmooth 0.5 tupleA)
-            (withPaint (always lime) 
-                (rightTri)))
-
- -- List Function that increments from 0 up to 600 in 50's               
+ -- List Function that increments from 0 up to 800 in 50's               
 listA :: [Length]
 listA = [0,50..800]
 
 -- Use listA values and wrap these in a tuple with 0 values
-tupleA ::[(Length, Length)]
-tupleA = [(hor,vert) | hor <- listA, vert <- [0]]
+moveRight ::[(Length, Length)]
+moveRight = [(hor,vert) | hor <- listA, vert <- [0]]
 
--- function of type polygon which is a right facing triangle
+-- function of type Animation which is a right facing triangle
 rightTri :: Animation
 rightTri = polygon [(0,0), (75,50), (0,100)]
 
+-- function of type Animation which is a left facing triangle
+leftTri :: Animation
+leftTri = polygon[(75,0), (0,50), (75,100)]
 
+-- func of Type Animation which is the left to right movement of lime coloured triangles - on different points
+triLimeLTR :: Animation
+triLimeLTR =
+    (combine
+    [translate 
+    (always (fromIntegral 0, fromIntegral a)) -- one animation
+    (translate
+        (repeatSmooth (0,0) [(x, (hor,ver)) | x <- [0,2..80], hor <- [800,750..0], ver <- [0]]) -- returns type Varying
+            (withPaint (always lime) 
+                (rightTri))) |
+              a <- [0,200..600]])
+
+-- func of Type Animation which is the right to left movement of lime coloured triangles              
+triLimeRTL :: Animation
+triLimeRTL = 
+    (combine
+    [translate 
+    (always (fromIntegral 0, fromIntegral a)) -- one animation
+    (translate
+        (repeatSmooth (0,0) [(x, (hor,ver)) | x <- [2,4], hor <- [0,50..800], ver <- [0]]) -- returns type Varying
+            (withPaint (always lime) 
+                (leftTri))) |
+              a <- [0,200..600]])
+
+
+
+-- func of type Animation which is right to left movment of teal triangles    
+triTealRTL :: Animation
+triTealRTL =
+    (combine
+    [translate 
+    (always (fromIntegral 0, fromIntegral a)) -- one animation
+    (translate
+        (repeatSmooth (800,0) [(x, (hor,ver)) | x <- [1,3], hor <- [0,50..800], ver <- [0]]) -- returns type Varying
+            (withPaint (always teal) 
+                (leftTri))) |
+              a <- [100,300..500]])
+
+-- func of type Animation which is right to left movment of teal triangles                  
+triTealLTR :: Animation
+triTealLTR = 
+    (combine
+    [translate 
+    (always (fromIntegral 0, fromIntegral a)) -- one animation
+    (translate
+        (repeatSmooth (800,0) [(x, (hor,ver)) | x <- [3,5], hor <- [800,750..0], ver <- [0]]) -- returns type Varying
+            (withPaint (always teal) 
+                (rightTri))) |
+              a <- [100,300..500]])
+              
+ 
 test :: IO ()
 test = writeFile "test.svg" (svg 800 600 picture)
